a754e851abc397fb1ed3df477a3fb7c071f56795
puzanov <puzanov@gmail.com>
2018-04-25 23:10:54 +0600

    initial, working cli intergace

---
+++ cli.rb
@@ -0,0 +1,13 @@
+require "bundler/setup"
+require "cryptoexchange"
+
+client = Cryptoexchange::Client.new
+dce = ARGV[0]
+
+begin
+    pairs = client.pairs(dce)
+    pairs.each {|p|
+        puts p.base + "-" +p.target
+    }
+rescue
+end
