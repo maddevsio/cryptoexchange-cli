6a3d9d4af8ad074adccd41b0c69ded9591be32d9
Mirbek Askerov <mirbek.askerov@yandex.ru>
2020-05-18 23:02:25 +0600

    Rubocop fixes

--- cli.rb
+++ cli.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "bundler/setup"
-require "cryptoexchange"
+require 'bundler/setup'
+require 'cryptoexchange'
 
 client = Cryptoexchange::Client.new
 dce = ARGV[0]
 
 begin
-    pairs = client.pairs(dce)
-    pairs.each {|p|
-        puts p.base + "-" +p.target
-    }
+  pairs = client.pairs(dce)
+  pairs.each { |p| puts "#{p.base}-#{p.target}" }
 rescue
 end
